fba1d5e7c116d13238559a51f4d23ceabd921d4f
Ubuntu <ubuntu@ip-172-31-36-99.ec2.internal>
2019-11-26 21:58:29 +0000

    Created stylesheet + templates + began nav development

--- app.py
+++ app.py
@@ -14,7 +14,33 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
-    return render_template("index.html", recipes=mongo.db.recipes.find())
+    ''' function to display the landing page with all recipes '''
+    
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    ''' function to display the about page '''
+    
+    return render_template('about.html')
+    
+
+@app.route('/sign_up')
+def sign_up():
+    ''' function to display the sign up page with a form for 
+        users to create an account '''
+    
+    return render_template('sign_up.html')
+
+
+@app.route('/login')
+def login():
+    ''' function to display the login page with a form for 
+        users to enter their details '''
+    
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
