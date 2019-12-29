59f5e444178a1cdd749934c0b16b1a542f4d8391
Ubuntu <ubuntu@ip-172-31-36-99.ec2.internal>
2019-12-29 22:09:08 +0000

    Created Recipe form to add a recipe

--- forms.py
+++ forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, \
+    IntegerField, SelectField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 class SignupForm(FlaskForm):
@@ -11,8 +12,30 @@ class SignupForm(FlaskForm):
     submit = SubmitField('Sign Up')
     
 class LoginForm(FlaskForm):
-    username = StringField('Username',
-                            validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField('Username', validators=[DataRequired(), 
+                            Length(min=2, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+    
+class RecipeForm(FlaskForm):
+    recipe_name = StringField('Recipe Name', validators=[DataRequired(), 
+                              Length(min=2, max=20)])
+    summary = StringField('Recipe Summary', validators=[DataRequired(), 
+                              Length(min=2, max=50)])
+    picture = StringField('Recipe Photo URL', validators=[DataRequired()])
+    ingredients = TextAreaField('Ingredients e.g Peas, Carrots', validators=[DataRequired(),
+                                Length(min=2)])
+    equipment = TextAreaField('Equipment e.g Saucepan, Fork', validators=[DataRequired(),
+                                Length(min=2)])
+    prep_time = IntegerField('Prep Time in mins e.g 5', validators=[DataRequired(), 
+                              Length(min=1, max=3)])
+    cook_time = IntegerField('Cook Time in mins e.g 5', validators=[DataRequired(), 
+                              Length(min=1, max=3)])
+    method = TextAreaField('Method (Seperate points with commas)', validators=[DataRequired(),
+                              Length(min=2)])
+    course = SelectField(u'Course', choices=[
+        ('breakfast', 'Breakfast'),
+        ('lunch', 'Lunch'),
+        ('dinner', 'Dinner'),
+        ])
+    submit_add = SubmitField('Add Recipe')
