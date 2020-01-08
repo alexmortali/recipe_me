450fdee9e1ab04bbda451ba92073b0978178031d
Ubuntu <ubuntu@ip-172-31-36-99.ec2.internal>
2020-01-08 14:43:24 +0000

    Pep8 Changes

---
+++ test_routes.py
@@ -0,0 +1,33 @@
+from app import app
+import unittest
+
+
+class FlaskTestApp(unittest.TestCase):
+
+    def test_index_page_loads(self):
+        response = app.test_client(self).get('/',
+                                             content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_loads(self):
+        response = app.test_client(self).get('/about',
+                                             content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page_loads(self):
+        response = app.test_client(self).get('/login',
+                                             content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_page_loads(self):
+        response = app.test_client(self).get('/sign_up',
+                                             content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_addrecipe_page_loads(self):
+        response = app.test_client(self).get('/addrecipe',
+                                             content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
